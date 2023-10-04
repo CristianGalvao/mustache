14274da1ca834f67d9303a0c9423aa2ff4208162
CristianGalvao <cristian2018galvao@outlook.com>
2023-10-03 23:05:55 -0300

    Add loadHome

--- views/public/script/script.js
+++ views/public/script/script.js
@@ -3,44 +3,45 @@ const sidebar = document.querySelector(".sidebar");
 const toggle = document.querySelector(".toggle");
 const searchBtn = document.querySelector(".search-box");
 const navlink = document.querySelectorAll('.nav-link');
+const home = document.querySelector(".loadHome");
+
+const loadHome = document.querySelector(".teste");
+
+window.onload = function () {
+  openCity(event, 'Home')
+  home.className = 'nav-link active'
+  console.log(home)
+};
 
 
-toggle.addEventListener("click", ()=>{
-    sidebar.classList.toggle("close")
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close")
 });
 
 
-function OpenMenuLink(){
-    for (let i of navlink){
-        i.classList.remove('active');
-    }
-    this.classList.add("active");
-    
-     
-}
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-for (let i of navlink){
-    i.addEventListener("click", OpenMenuLink)
-}
-
-
-  function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("home");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("nav-link");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("home");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("nav-link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+// LOGOUT
+function formLogout() {
+  document.getElementById("form-logout").submit();
+}
